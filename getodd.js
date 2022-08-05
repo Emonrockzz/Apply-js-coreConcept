d9172ad3b2ccead54531ac106cf94e3c7ab3fec0
Emonrockzz <Emonrockzz121@gmail.com>
2022-08-05 15:11:09 +0600

    removde debugh

---
+++ getodd.js
@@ -0,0 +1,30 @@
+function getSumofAnArry(numbers){
+    let sum = 0;
+    for(let i=0; i< numbers.length; i++){
+        const index = i;
+        const element = numbers[index];
+        sum = sum + element;
+        console.log(index, element, sum);
+    }
+    return sum;
+}
+function getOddNumbersOfAnArray(numbers){
+    const oddNumbers = [];
+    for(let i =0; i < numbers.length; i++){
+        const index = i;
+        const element = numbers[index];
+        if(element % 2 !== 0){
+            console.log(index, element);
+            oddNumbers.push(element)
+        }
+    }
+    return oddNumbers;
+}
+
+const myNumbers = [12, 65, 45, 78, 32, 46, 91, 75, 71, 80];
+const oddNumbers = getOddNumbersOfAnArray(myNumbers);
+console.log(oddNumbers);
+const oddNumbersSum = getOddNumbersOfAnArray(oddNumbers);
+console.log('odd number sum', oddNumbersSum)
+
+// unsolved 2
